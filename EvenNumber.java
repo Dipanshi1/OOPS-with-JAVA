b92c5be18bb13dc58a8f2df9cb1bf619d9ec9232
Dipanshi1 <dipanshig6969@gmail.com>
2026-02-03 10:17:12 +0530

    Added even number by loops

---
+++ EvenNumber.java
@@ -0,0 +1,12 @@
+public class EvenNumber{
+    public static void main(String[] args){
+        for(int i = 2; i<=10; i+=1){
+            if (i%2==0){
+                System.out.println(i + " is an even number");
+            }else{
+                System.out.println(i + " is an odd number");
+            }
+        }
+    }
+}
+
